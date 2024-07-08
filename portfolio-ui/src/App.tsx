798177c5ab353ad814f9db3eff690fc2108a6e17
unknown <pyudhistira02@gmail.com>
2024-07-08 14:30:28 -0400

    tailwind stuff

--- portfolio-ui/src/App.tsx
+++ portfolio-ui/src/App.tsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import { Route } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
+import ProfileCard from './Components/ProfileCard'
 
 function App() {
   return (
-    <>
-    </>
+      <>
+        <ProfileCard />
+      </>
   )
 }
 
---
+++ portfolio-ui/src/Components/ProfileCard.tsx
@@ -0,0 +1,14 @@
+
+
+
+function ProfileCard() {
+    return (
+        <div className="card">
+            <img src="https://via.placeholder.com/150" alt="profile" />
+            <h2>John Doe</h2>
+            <p>Software Engineer</p>
+        </div>
+    );    
+}
+
+export default ProfileCard;
